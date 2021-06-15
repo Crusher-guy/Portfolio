698be58ce81257b04e52b5ce0bbf72d28254f39d
leokattah <leokattah@gmail.com>
2021-06-15 17:41:11 -0300

    modal certificates  started

--- js/scriptsModalCertificados.js
+++ js/scriptsModalCertificados.js
@@ -2,37 +2,33 @@
 var modal = document.getElementById('modal');
 
 // Get the button that opens the modal
-var btn = document.getElementById('saiba_mais');
+var btn = document.getElementById('saiba_mais_certificado');
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName('fechar')[0];
+var span = document.getElementsByClassName('fechar_certificado')[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
-  document.querySelector('.modal-overlay').classList.add('active');
+  document.querySelector('.modal-overlay-certificado').classList.add('active');
 };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  document.querySelector('.modal-overlay').classList.remove('active');
+  document.querySelector('.modal-overlay-certificado').classList.remove('active');
 };
 
 // When user clicks anywhere outside of the modal, close it
 // span.addEventListener('click', function (event) {
 //   if (event.target == modal) {
-//     document.querySelector('.modal-overlay').classList.remove('active');
+//     document.querySelector('.modal-overlay-certificado').classList.remove('active');
 //   }
 // });
 
 window.onclick = function(event) {
   if (event.target == modal) {
-    document.querySelector('.modal-overlay').classList.remove('active');
+    document.querySelector('.modal-overlay-certificado').classList.remove('active');
   }
   if (event.target == modal) {
-    document.querySelector('.modal-overlay').classList.remove('active');
+    document.querySelector('.modal-overlay-certificado').classList.remove('active');
   }
 }
-
-
-// Modal do certificado
-
