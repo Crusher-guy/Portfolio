37bf88f4f2563a63db0b47f12ede56b52163ba47
leokattah <leokattah@gmail.com>
2021-06-16 19:49:27 -0300

    courses done

---
+++ js/scriptsModalcurso.js
@@ -0,0 +1,34 @@
+// Get the modal
+var modal = document.getElementById('modal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('saiba_mais_curso');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('fechar_curso')[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  document.querySelector('.modal-overlay-curso').classList.add('active');
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  document.querySelector('.modal-overlay-curso').classList.remove('active');
+};
+
+// When user clicks anywhere outside of the modal, close it
+// span.addEventListener('click', function (event) {
+//   if (event.target == modal) {
+//     document.querySelector('.modal-overlay-certificado').classList.remove('active');
+//   }
+// });
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    document.querySelector('.modal-overlay-curso').classList.remove('active');
+  }
+  if (event.target == modal) {
+    document.querySelector('.modal-overlay-curso').classList.remove('active');
+  }
+}
