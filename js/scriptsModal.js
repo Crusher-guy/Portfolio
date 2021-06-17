e010c5e2a0198b81c0b9a218fc952a8def118a12
leokattah <leokattah@gmail.com>
2021-06-17 14:33:11 -0300

    modal portfolios cards in progress

--- js/scriptsModal.js
+++ js/scriptsModal.js
@@ -294,3 +294,43 @@ window.onclick = function(event) {
     document.querySelector('.modal-overlay-card8').classList.remove('active');
   }
 }
+
+
+
+
+//  Modal saiba mais portfolio card9
+// Get the modal from portólio
+var modal = document.getElementById('modal-card9');
+
+// Get the button that opens the modal
+var btn = document.getElementById('saiba_mais_portfolio_card9');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('fechar-card9')[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  document.querySelector('.modal-overlay-card9').classList.add('active');
+};
+
+// When the user clicks on <span> (x), close the modal 
+span.onclick = function () {
+  document.querySelector('.modal-overlay-card9').classList.remove('active');
+};
+
+// When user clicks anywhere outside of the modal, close it
+// span.addEventListener('click', function (event) {
+//   if (event.target == modal) {
+//     document.querySelector('.modal-overlay-card9').classList.remove('active');
+//   }
+// });
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    document.querySelector('.modal-overlay-card9').classList.remove('active');
+  }
+  if (event.target == modal) {
+    document.querySelector('.modal-overlay-card9').classList.remove('active');
+  }
+}
+
