8e8ba8969784f6f4869f72f7df31dd3ee26e6cea
leokattah <leokattah@gmail.com>
2021-06-19 17:22:44 -0300

    contact form in progress

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
-console.log("olá")
+
+const contactForm = document.querySelector(".contact-form");
+
+// Função para evitar que a página seja recarregada quando o botao "enviar" for clicado.
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log("enviar clicado")
+})
+
