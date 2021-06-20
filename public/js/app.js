99c02e7459c222ed4c323cf4b70ccc2140ed0ff8
leokattah <leokattah@gmail.com>
2021-06-20 12:37:38 -0300

    contact form done

--- public/js/app.js
+++ public/js/app.js
@@ -10,29 +10,29 @@ contactForm.addEventListener('submit', (e) => {
 
   // Construção de um objeto JS
   let formData = {
-    name: nome.value,
+    nome: nome.value,
     email: email.value,
-    message: email.value,
+    message: message.value,
   };
 
   // Depois de criar um objeto com os dados digitados no formulário, enviarei para o backend com AJAX
   let xhr = new XMLHttpRequest();
-  xhr.open('POST', '/'); // Primeiro argumento é o método POST, usado enviar os dados. O segundo argumento é '/' é a página de onde os dados serão tirados. 
-  xhr.setRequestHeader('content-type', 'aplication/JSON'); // aplication/json é a melhor forma de converter os dados e enviar, considerando que os dados capturados foi inseridos no objeto acima. 
-  xhr.onload = function(){
+  xhr.open('POST', '/'); // Primeiro argumento é o método POST, usado enviar os dados. O segundo argumento é '/' é a página de onde os dados serão tirados.
+  xhr.setRequestHeader('content-type', 'application/JSON'); // application/json é a melhor forma de converter os dados e enviar, considerando que os dados capturados foi inseridos no objeto acima.
+  xhr.onload = function () {
     console.log(xhr.responseText);
-    if(xhr.responseText == "sucess"){
-    alert ('Email enviado com sucesso! Entrarei em contato em breve.');
-    nome.value = ''; // Este comando limpa o campo depois de o botão "enviar" ser clicado. 
-    email.value = ''; // Este comando limpa o campo depois de o botão "enviar" ser clicado. 
-    message.value = ''; // Este comando limpa o campo depois de o botão "enviar" ser clicado. 
-  }else{
-    alert('Algo não saiu como esperado e seu e-mail não foi enviado. Se preferir faça contato pelo número whatsapp disponível na página')
-  }
-}
+    if(xhr.responseText == 'Email enviado com sucesso!') {
+      alert('Email enviado com sucesso! Entrarei em contato em breve.');
+      nome.value = ''; // Este comando limpa o campo depois de o botão "enviar" ser clicado.
+      email.value = ''; // Este comando limpa o campo depois de o botão "enviar" ser clicado.
+      message.value = ''; // Este comando limpa o campo depois de o botão "enviar" ser clicado.
+    }else {
+      alert(
+        'Algo não saiu como esperado e seu e-mail não foi enviado. Se preferir faça contato pelo número whatsapp disponível na página'
+      );
+    }
+  };
 
-// Enviando os dados da variável formData para o backend como string. 
-xhr.send(JSON.stringify(formData));
-
-})
-
+  // Enviando os dados da variável formData para o backend como string.
+  xhr.send(JSON.stringify(formData));
+});
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require("express"); // imporando o pacote.
+const express = require('express'); // imporando o pacote.
 const app = express(); // inicializandno o pacote.
+const nodemailer = require('nodemailer'); //importando o pacote.
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,14 +9,49 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('public')); // Indicando qual é a pasta que os arquivos estáticos estão.
 app.use(express.json());
 
-app.get ('/', (req, res) => {
-  res.sendFile(__dirname + "/public/index.html") // Indicando qual é o caminho do arquivo princial
-})
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html'); // Indicando qual é o caminho do arquivo princial. Ele que será aberto no browser.
+});
 
-app.post('/', (req,res) => {
-  console.log(req.body)
-})
+app.post('/', (req, res) => {
+  console.log(req.body);
+
+  // Agora é hora de enviar os dados para o GMAIL. O método abaixo está descrito na documentação nodemailer.
+  // const transporter = nodemailer.createTransport({
+  //   service: 'gmail',
+  //   auth: {
+  //     user: 'leokattah@gmail.com',
+  //     pass: 'Subway-01'
+  //   },
+  // });
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587, // port for secure SMTP
+    secureConnection: false, // TLS requires secureConnection to be false
+    auth: {
+      user: 'devleokattah@gmail.com',
+      pass: 'Brasil-01',
+    },
+  });
+
+  const mailOptions = {
+    from: req.body.email,
+    to: 'devleokattah@gmail.com',
+    subject: `Mensagem de  ${req.body.nome}: ${req.body.email}`,
+    text: req.body.message, 
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('error');
+    } else {
+      console.log('Email enviado:' + info.response);
+      res.send('Email enviado com sucesso!');
+    }
+  });
+});
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`)
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
