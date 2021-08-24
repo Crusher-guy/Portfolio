057b40e523bf02106aa7cef296406573908fcdc9
leokattah <leokattah@gmail.com>
2021-08-24 04:06:34 -0300

    img resize updated

--- public/js/scriptsModal.js
+++ public/js/scriptsModal.js
@@ -547,3 +547,41 @@ window.onclick = function(event) {
     document.querySelector('.modal-overlay-card15').classList.remove('active');
   }
 }
+
+
+
+//  Modal saiba mais portfolio card16
+// Get the modal from portólio
+var modal = document.getElementById('modal-card16');
+
+// Get the button that opens the modal
+var btn = document.getElementById('saiba_mais_portfolio_card16');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('fechar-card16')[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  document.querySelector('.modal-overlay-card16').classList.add('active');
+};
+
+// When the user clicks on <span> (x), close the modal 
+span.onclick = function () {
+  document.querySelector('.modal-overlay-card16').classList.remove('active');
+};
+
+// When user clicks anywhere outside of the modal, close it
+// span.addEventListener('click', function (event) {
+//   if (event.target == modal) {
+//     document.querySelector('.modal-overlay-card16').classList.remove('active');
+//   }
+// });
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    document.querySelector('.modal-overlay-card16').classList.remove('active');
+  }
+  if (event.target == modal) {
+    document.querySelector('.modal-overlay-card16').classList.remove('active');
+  }
+}
